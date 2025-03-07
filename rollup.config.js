859718515a829bdc84ef19fbe664baa299f112e6
Shane Holloway <shane.holloway@ieee.org>
2025-03-07 08:06:41 -0700

    Split apart unittest groupings

--- rollup.config.js
+++ rollup.config.js
@@ -3,52 +3,59 @@ import rpi_resolve from '@rollup/plugin-node-resolve'
 
 const external = id => /^\w+:|^#/.test(id)
 
-const _rpis_ = (defines) => [
-  rpi_jsy({defines}),
-  rpi_resolve(),
-]
-
 export const pkg_cfg = {
-  plugins: _rpis_({NO_CBOR:true}),
+  plugins: [ rpi_jsy(), rpi_resolve() ],
   external,
 
-  input: [
-    './code/index.jsy',
-    './code/subtle.jsy',
-
-    './code/basic/opaque_basic.jsy',
-    './code/basic/opaque_basic_hmac.jsy',
-
-    './code/opaque_tahoe.jsy',
-    './code/opaque_composite.jsy',
-
-    './code/ecdsa/opaque_ecdsa_basic.jsy',
-    './code/ecdsa/opaque_ecdsa_tahoe.jsy',
-
-    './code/ecdhe/opaque_ecdhe.jsy',
-    './code/ecdhe/opaque_ecdhe_basic.jsy',
-    './code/ecdhe/opaque_ecdhe_tahoe.jsy',
-  ],
   output: {
     dir: 'esm',
     format: 'es',
     sourcemap: true,
-    chunkFileNames: '[name].js'
-  }
+  },
+
+  input: {
+    'index': './code/index.jsy',
+    'subtle': './code/subtle.jsy',
+    'subtle/iso_aes_256_gcm': './code/subtle/iso_aes_256_gcm.jsy',
+    'subtle/iso_ecdh': './code/subtle/iso_ecdh.jsy',
+    'subtle/iso_ecdhe': './code/subtle/iso_ecdhe.jsy',
+    'subtle/iso_ecdsa': './code/subtle/iso_ecdsa.jsy',
+    'subtle/iso_hmac_sha2': './code/subtle/iso_hmac_sha2.jsy',
+    'subtle/iso_random': './code/subtle/iso_random.jsy',
+    'subtle/iso_sha2_hashes': './code/subtle/iso_sha2_hashes.jsy',
+    'subtle/utils': './code/subtle/utils.jsy',
+
+    'opaque_basic': './code/basic/opaque_basic.jsy',
+    'opaque_basic_hmac': './code/basic/opaque_basic_hmac.jsy',
+
+    'opaque_tahoe': './code/opaque_tahoe.jsy',
+    'opaque_composite': './code/opaque_composite.jsy',
+
+    'opaque_ecdsa_basic': './code/ecdsa/opaque_ecdsa_basic.jsy',
+    'opaque_ecdsa_tahoe': './code/ecdsa/opaque_ecdsa_tahoe.jsy',
+
+    'opaque_ecdhe': './code/ecdhe/opaque_ecdhe.jsy',
+    'opaque_ecdhe_basic': './code/ecdhe/opaque_ecdhe_basic.jsy',
+    'opaque_ecdhe_tahoe': './code/ecdhe/opaque_ecdhe_tahoe.jsy',
+  },
 }
 
 export const pkg_test_cfg = {
-  plugins: _rpis_({NO_CBOR:true}),
+  plugins: [ rpi_jsy(), rpi_resolve() ],
   external,
 
-  input: [
-    './test/unittest.jsy',
-  ],
   output: {
     dir: 'esm-test',
     format: 'es',
     sourcemap: true,
-  }
+  },
+
+  input: {
+    'test-subtle': './test/subtle/unittest.jsy',
+    'test-core': './test/core/unittest.jsy',
+    'test-ecdhe': './test/ecdhe/unittest.jsy',
+    'test-ecdsa': './test/ecdsa/unittest.jsy',
+  },
 }
 
 export default [
